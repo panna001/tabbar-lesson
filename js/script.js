fa57b0afd6936e74034e860daadd0a15fb0ff982
石川大地 <ishikawadaichi@daichinoMacBook-Air.local>
2021-01-27 20:11:25 +0900

    [Finish]js-12

--- js/script.js
+++ js/script.js
@@ -1,9 +1,21 @@
-$('#tab-contents .tab[id != "tab1"]').hide();
+API_KEY = '7e081589859a367b15d0c02500f8b2f1';
 
-$('#tab-menu a').on('click', function(event){
-  $('#tab-contents .tab').hide();
-  $('#tab-menu .active').removeClass('active');
-  $(this).addClass('active');
-  $($(this).attr('href')).show();
-  event.preventDefault();
+$(function(){
+  $('#btn').on('click', function(){
+    $.ajax({
+      url: "http://api.openweathermap.org/data/2.5/weather?q=" + $('#cityname').val() + "&units=metric&appid=" + API_KEY,
+      dataType : 'jsonp',
+    }).done(function(data){
+      $('#place').text(data.name);
+      $('#temp_max').text(data.main.temp_max);
+      $('#temp_min').text(data.main.temp_min);
+      $('#humidity').text(data.main.humidity);
+      $('#speed').text(data.wind.speed);
+      $('#weather').text(data.weather[0].main);
+      $('#img').attr("src","http://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
+      $('#img').attr("alt","data.weather[0].main");
+    }).fail(function(data){
+      alert('通信に失敗しました。');
+    });
+  });
 });
